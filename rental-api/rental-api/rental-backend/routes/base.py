1224992d66a89981b68f59edcc38548120ba3c2a
Zimovchik <63729114+Zimovchik@users.noreply.github.com>
2025-03-02 12:56:04 +0300

    initial

---
+++ rental-api/rental-api/rental-backend/routes/base.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi_sqlalchemy import DBSessionMiddleware
+from rental-backend import __version__
+from rental-backend.settings import get_settings
+
+settings = get_settings()
+app = FastAPI(
+    title='Сервис цифрового проката',
+    description='Краткое описание',
+    version=__version__,
+
+    # Отключаем нелокальную документацию
+    root_path=settings.ROOT_PATH if __version__ != 'dev' else '',
+    docs_url=None if __version__ != 'dev' else '/docs',
+    redoc_url=None,
+)
+
+
+app.add_middleware(
+    DBSessionMiddleware,
+    db_url=str(settings.DB_DSN),
+    engine_args={"pool_pre_ping": True, "isolation_level": "AUTOCOMMIT"},
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ALLOW_ORIGINS,
+    allow_credentials=settings.CORS_ALLOW_CREDENTIALS,
+    allow_methods=settings.CORS_ALLOW_METHODS,
+    allow_headers=settings.CORS_ALLOW_HEADERS,
+)
