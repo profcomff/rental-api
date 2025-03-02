44222f54d8e6c0fb9301662eddecb5e043691458
Zimovchik <63729114+Zimovchik@users.noreply.github.com>
2025-03-02 13:07:39 +0300

    rm unused

--- rental-api/rental-backend/__init__.py
+++
@@ -1,3 +0,0 @@
-import os
-
-__version__ = os.getenv("APP_VERSION", "dev")
--- rental-api/rental-backend/__main__.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-from rental-backend.routes.base import app
-
-if __name__ == '__main__':
-    uvicorn.run(app)
