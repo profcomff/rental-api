12df035ec8f48d40db6088082ea4efb3ef41982d
ValeryEstal <pavlovskaya_valery@mail.ru>
2025-03-27 00:09:14 +0300

    session

--- tests/test_routes/test_item.py
+++ tests/test_routes/test_item.py
@@ -1,7 +1,10 @@
 import pytest
 from starlette import status
 
-from rental_backend.models.db import Item
+from rental_backend.__main__ import app
+from rental_backend.models.db import Item, ItemType
+from rental_backend.schemas.base import StatusResponseModel
+from rental_backend.schemas.models import ItemGet, ItemPost
 
 
 url = '/item'
