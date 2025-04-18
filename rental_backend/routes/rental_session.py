47cede2eb49aa7f43d6a757978eebde8d3a8a1e0
ValeryEstal <pavlovskaya_valery@mail.ru>
2025-04-18 16:51:50 +0300

    cancel session

--- rental_backend/routes/rental_session.py
+++ rental_backend/routes/rental_session.py
@@ -221,11 +221,6 @@ async def get_rental_session(session_id: int, user=Depends(UnionAuth())):
     return RentalSessionGet.model_validate(session)
 
 
-# давай напишем краткое описание в функции, что она делает.
-# Что-то на подобии: """Отменяет сессию в статусе reserved.
-# Отменить может только сам юзер"""
-
-
 @rental_session.delete("/{session_id}/cancel", response_model=RentalSessionGet)
 async def cancel_rental_session(session_id: int, user=Depends(UnionAuth())):
     """Отменяет сессию в статусе RESERVED. Отменить может только сам юзер
