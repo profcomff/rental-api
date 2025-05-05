1fdc04d65f214d41b255a67e5759b087cca984ec
petrCher <88943157+petrCher@users.noreply.github.com>
2025-05-05 23:17:27 +0300

    black and isort changes

--- tests/test_routes/test_item.py
+++ tests/test_routes/test_item.py
@@ -2,8 +2,10 @@ import logging
 
 import pytest
 from starlette import status
-from rental_backend.settings import get_settings
+
 from rental_backend.models import Item
+from rental_backend.settings import get_settings
+
 
 logger = logging.getLogger(__name__)
 url: str = '/item'
@@ -13,13 +15,14 @@ settings = get_settings()
 
 @pytest.mark.parametrize('response_status', [status.HTTP_200_OK, status.HTTP_404_NOT_FOUND])
 def test_create_item(client, item_type_fixture, response_status):
-    if response_status==status.HTTP_200_OK:
+    if response_status == status.HTTP_200_OK:
         body = {"type_id": item_type_fixture.id, "is_available": True}
-    #check non-existing type_id request
-    elif response_status==status.HTTP_404_NOT_FOUND:
-        body = {"type_id": item_type_fixture.id-1, "is_available": True}
+    # check non-existing type_id request
+    elif response_status == status.HTTP_404_NOT_FOUND:
+        body = {"type_id": item_type_fixture.id - 1, "is_available": True}
     post_response = client.post(url, json=body)
     assert post_response.status_code == response_status
+
 
 @pytest.mark.parametrize(
     'item_n, response_status',
@@ -27,9 +30,7 @@ def test_create_item(client, item_type_fixture, response_status):
 )
 def test_get_item_id(client, dbsession, items_with_types, item_n, response_status):
     items = items_with_types
-    item = (
-        dbsession.query(Item).filter(Item.id == items[item_n].id).one_or_none()
-    )
+    item = dbsession.query(Item).filter(Item.id == items[item_n].id).one_or_none()
     print(item)
     # check non-existing id request
     item_id = -1
