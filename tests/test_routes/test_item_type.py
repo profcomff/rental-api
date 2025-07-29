60cf8c1328040a26089c0840d984d181434f6207
petrCher <88943157+petrCher@users.noreply.github.com>
2025-07-29 20:47:38 +0300

    item_type autotests

---
+++ tests/test_routes/test_item_type.py
@@ -0,0 +1,66 @@
+import logging
+
+import pytest
+from starlette import status
+
+from rental_backend.settings import get_settings
+
+
+logger = logging.getLogger(__name__)
+url: str = '/itemtype'
+
+settings = get_settings()
+
+
+@pytest.mark.parametrize(
+    'item_n,response_status',
+    [
+        (0, status.HTTP_200_OK),
+        (1, status.HTTP_200_OK),
+        (2, status.HTTP_422_UNPROCESSABLE_ENTITY),
+    ],
+)
+def test_create_item_type(client, item_type_fixture, item_n, response_status):
+    body = None
+    if item_n < len(item_type_fixture):
+        body = {"name": item_type_fixture[item_n].name}
+    post_response = client.post(url, json=body)
+    assert post_response.status_code == response_status
+
+
+@pytest.mark.parametrize('response_status', [status.HTTP_200_OK])
+def test_get_item_type(client, item_type_fixture, response_status):
+    get_response = client.get(url)
+    assert get_response.status_code == response_status
+
+
+@pytest.mark.parametrize(
+    'item_n,response_status',
+    [
+        (0, status.HTTP_200_OK),
+        (1, status.HTTP_200_OK),
+        (2, status.HTTP_404_NOT_FOUND),
+    ],
+)
+def test_get_item_type_id(client, item_n, item_type_fixture, response_status):
+    type_id = -1
+    if item_n < len(item_type_fixture):
+        type_id = item_type_fixture[item_n].id
+    response = client.get(f'{url}/{type_id}')
+    assert response.status_code == response_status
+
+
+@pytest.mark.parametrize('response_status', [status.HTTP_200_OK])
+def test_update_item_type(client, item_type_fixture, dbsession, response_status):
+    pass
+
+
+def test_delete_item_type(client, item_type_fixture):
+    response = client.delete(f"{url}/{item_type_fixture[0].id}")
+    assert response.status_code == status.HTTP_200_OK
+    # trying to delete deleted
+    response = client.delete(f"{url}/{item_type_fixture[0].id}")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    # trying to get deleted
+    response = client.get(f'{url}/{item_type_fixture[0].id}')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
