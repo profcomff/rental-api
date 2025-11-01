4eba0161f23f6f4786e91c9cac5f0a9614e3066f
Yarmoshch <yaroslav2006@inbox.ru>
2025-11-01 15:41:25 +0300

    lint

--- migrations/versions/498e4ee72e00_add_session_id.py
+++ migrations/versions/498e4ee72e00_add_session_id.py
@@ -20,6 +20,7 @@ def upgrade():
     op.add_column('strike', sa.Column('session_id', sa.Integer(), nullable=True))
     op.create_foreign_key('strike_session_id_fkey', 'strike', 'rental_session', ['session_id'], ['id'])
 
+
 def downgrade():
     op.drop_constraint('strike_session_id_fkey', 'strike', type_='foreignkey')
     op.drop_column('strike', 'session_id')
