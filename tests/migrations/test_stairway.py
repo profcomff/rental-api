5e988db6894920de94a8add9f6c6fd1ec388d20b
Yarmoshch <yaroslav2006@inbox.ru>
2025-11-01 15:38:33 +0300

    downgrade migration fix and stairway test added

---
+++ tests/migrations/test_stairway.py
@@ -0,0 +1,22 @@
+import subprocess
+import pytest
+
+def get_all_revisions():
+    result = subprocess.run(["alembic", "history"], stdout=subprocess.PIPE, check=True)
+    lines = result.stdout.decode().splitlines()
+    revisions = []
+    for line in lines:
+        if line and "->" in line:
+            rev = line.split("->")[0].strip()
+            # if rev != "<base>":
+            #     revisions.append(rev)
+    return list(reversed(revisions))
+
+@pytest.mark.parametrize("rev", get_all_revisions())
+def test_stairway_upgrade_downgrade(rev):
+    # upgrade к ревизии
+    subprocess.check_call(["alembic", "upgrade", rev])
+    # downgrade на шаг
+    subprocess.check_call(["alembic", "downgrade", "-1"])
+    # upgrade обратно
+    subprocess.check_call(["alembic", "upgrade", rev])
