6214ab0e626bd7c7dca2fd40299fb544efae92ca
petrCher <88943157+petrCher@users.noreply.github.com>
2025-12-02 22:07:28 +0300

    changed sorting

--- rental_backend/routes/rental_session.py
+++ rental_backend/routes/rental_session.py
@@ -371,7 +371,7 @@ async def get_rental_sessions_common(
             RentStatus.RETURNED: 7,
         }
     status_order = case(status_to_show, value=RentalSession.status)
-    query = query.order_by(status_order, RentalSession.reservation_ts)
+    query = query.order_by(status_order, RentalSession.reservation_ts.desc())
 
     if user_id != 0:
         query = query.filter(RentalSession.user_id == user_id)
