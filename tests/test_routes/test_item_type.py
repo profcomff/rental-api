1ebbcef930e7fa79b07d816b886f98ab042a9be7
Artem Bratyashin <bratiashin.aa21@physics.msu.ru>
2025-12-13 20:34:18 +0300

    Исправил тесты

--- tests/test_routes/test_item_type.py
+++ tests/test_routes/test_item_type.py
@@ -28,17 +28,13 @@ def test_create_item_type(client, item_type_fixture, item_n, response_status):
     assert post_response.status_code == response_status
 
 
-def test_get_item_type_successfully(client, item_type_fixture):
+def test_get_item_type(client, item_type_fixture):
     get_response = client.get(url)
     assert get_response.status_code == status.HTTP_200_OK
-
-
-def test_get_item_type_unsuccessfully(client, item_type_fixture):
     response = client.delete(f"{url}/{item_type_fixture[0].id}")
     assert response.status_code == status.HTTP_200_OK
     response = client.delete(f"{url}/{item_type_fixture[1].id}")
     assert response.status_code == status.HTTP_200_OK
-    response = client.delete(f"{url}/{item_type_fixture[0].id}")
     get_response = client.get(url)
     assert get_response.status_code == status.HTTP_404_NOT_FOUND
 
@@ -57,11 +53,6 @@ def test_get_item_type_id(client, item_n, item_type_fixture, response_status):
         type_id = item_type_fixture[item_n].id
     response = client.get(f'{url}/{type_id}')
     assert response.status_code == response_status
-
-
-def test_get_item_type_id_invalid(client, item_type_fixture):
-    response = client.get(f'{url}/invalid')
-    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
 @pytest.mark.parametrize(
@@ -117,37 +108,24 @@ def test_update_item_type_available(client, item_n, count, items_with_same_type_
     [
         (0, status.HTTP_200_OK),
         (1, status.HTTP_200_OK),
+        (2, status.HTTP_404_NOT_FOUND),
     ],
 )
 def test_delete_item_type(client, item_type_fixture, item_n, response_status):
-    type_id = item_type_fixture[item_n].id
+    type_id = -100000
+    if item_n < len(item_type_fixture):
+        type_id = item_type_fixture[item_n].id
     response = client.delete(f"{url}/{type_id}")
     assert response.status_code == response_status
     if response.status_code == status.HTTP_200_OK:
-        assert client.get(f"{url}/{type_id}").status_code == status.HTTP_404_NOT_FOUND
+        response = client.get(f"{url}/{type_id}")
+        assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 def test_delete_item_type_with_items(client, items_with_same_type_id):
     type_id = items_with_same_type_id[0].id
     response = client.delete(f"{url}/{type_id}")
+    # Есть неудаленные items для данного item_type
     assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert client.get(f"{url}/{type_id}").status_code == status.HTTP_200_OK
-
-
-def test_delete_nonexistent_item_type(client, item_type_fixture):
-    response = client.delete(f"{url}/999999")
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-
-
-@pytest.mark.parametrize(
-    'type_id,response_status',
-    [
-        # Неверный id
-        (999999, status.HTTP_404_NOT_FOUND),
-        # Неверный ввод
-        ("invalid", status.HTTP_422_UNPROCESSABLE_ENTITY),
-    ],
-)
-def test_delete_item_type_errors(client, item_type_fixture, type_id, response_status):
-    response = client.delete(f"{url}/{type_id}")
-    assert response.status_code == response_status
+    response = client.get(f"{url}/{type_id}")
+    assert response.status_code == status.HTTP_200_OK
