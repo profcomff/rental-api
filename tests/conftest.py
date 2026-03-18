996b92df423ba15ea7774d5617e83c5dbb0a8d22
Georgon <43513853+Georgon@users.noreply.github.com>
2026-03-19 00:50:47 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -93,8 +93,8 @@ def authlib_user():
     return {
         "auth_methods": ["string"],
         "session_scopes": [{"id": 0, "name": "string"}],
-        "user_scopes": [{"id": 1, "name": "rental.session.admin"}],  # добавлен нужный скоуп "rental.session.admin" (по сути сейчас эта строка ничего не делает, но как в UnionAuth)
-        "scopes": ["rental.session.admin"],  # добавлено для корректной работы прав в тесте test_admin_can_update_any_rental_session
+        "user_scopes": [{"id": 1, "name": "rental.session.admin"}],  
+        "scopes": ["rental.session.admin"], 
         "indirect_groups": [0],
         "groups": [0],
         "id": 0,
