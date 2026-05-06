3f4fada15b37981f0fda4c8749712c5637aa3d58
NamazovMaksim <aslimbo2001@gmail.com>
2026-05-06 21:48:41 +0300

    добавлен тест test_get_items_positive_cases для проверки позитивных кейсов

--- tests/test_routes/test_item.py
+++ tests/test_routes/test_item.py
@@ -50,10 +50,42 @@ def test_get_item_id(client, dbsession, items_with_types, item_n, response_statu
     'item_n,response_status',
     [(0, status.HTTP_200_OK), (1, status.HTTP_200_OK)],
 )
-def test_get_items_by_type_id(client, items_with_types, item_n, response_status):
+def test_get_items_by_type_id(client, items_with_types, item_n, response_status): 
     query = {"type_id": f'{items_with_types[item_n].type_id}'}
     response = client.get(f'{url}', params=query)
     assert response.status_code == response_status
+
+@pytest.mark.parametrize(
+    "type_id, order_by, order, is_available, response_status",
+    [(0, None, None, True, status.HTTP_200_OK),
+     (0, "id", None, True, status.HTTP_200_OK),
+     (0, "type_id",	"asc",	False, status.HTTP_200_OK),
+     (0, "is_available", "desc", False, status.HTTP_200_OK),
+     (0, None, None, False, status.HTTP_200_OK),
+     (1, "id", "asc", False, status.HTTP_200_OK),
+     (1, "type_id", "desc", True, status.HTTP_200_OK),
+     (1, "is_available", None, True, status.HTTP_200_OK),
+     (1, None, "asc", True, status.HTTP_200_OK),
+     (2, "id", "desc", True, status.HTTP_200_OK),
+     (2, "type_id", None, False, status.HTTP_200_OK),
+     (2, "is_available", "asc", False, status.HTTP_200_OK),
+     (2, None, "desc", False, status.HTTP_200_OK),
+     (None, "id", None, True, status.HTTP_200_OK),
+     (None, "type_id", "asc", False, status.HTTP_200_OK),
+     (None, "is_available", "desc", False, status.HTTP_200_OK),
+     (None, "is_available", "desc", False, status.HTTP_200_OK),
+        ]
+    )
+def test_get_items_positive_cases(client, type_id, order_by, order, is_available, response_status):
+    dict_of_params = {"type_id" : type_id,
+                      "order_by" : order_by,
+                      "order" : order,
+                      "is_availible" : str(is_available).lower() if is_available is not None else None
+                      }
+    query = {k : v for k, v in dict_of_params.items() if v is not None}
+    response = client.get(url, params=query)
+    assert response.status_code == response_status
+
 
 
 @pytest.mark.parametrize(
